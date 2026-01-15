dc3740c58964bf4906537f4da0f4d7c5c9253c15
Saul Hanson <3pathstoperfection@gmail.com>
2026-01-15 16:35:51 -0700

    Added a shader class and fixed typo bug

---
+++ LearnOpenGL/Shader.h
@@ -0,0 +1,100 @@
+#pragma once
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+	unsigned int ID;
+
+	Shader(const char* vertPath, const char* fragPath) 
+	{
+		std::string vertSrc = LoadShaderFile(vertPath);
+		std::string fragSrc = LoadShaderFile(fragPath);
+
+		const char* vertCode = vertSrc.c_str();
+		const char* fragCode = fragSrc.c_str();
+
+		unsigned int vertex, fragment;
+
+		vertex = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertex, 1, &vertCode, NULL);
+		glCompileShader(vertex);
+		checkCompileErrors(vertex, "VERTEX");
+
+		fragment = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragment, 1, &fragCode, NULL);
+		glCompileShader(fragment);
+		checkCompileErrors(fragment, "FRAGMENT");
+
+		ID = glCreateProgram();
+		glAttachShader(ID, vertex);
+		glAttachShader(ID, fragment);
+		glLinkProgram(ID);
+		checkCompileErrors(ID, "PROGRAM");
+
+		glDeleteShader(vertex);
+		glDeleteShader(fragment);
+	}
+
+	void use()
+	{
+		glUseProgram(ID);
+	}
+	void setBool(const std::string& name, bool value) const
+	{
+		glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+	}
+	void setInt(const std::string& name, int value) const
+	{
+		glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+	}
+	void setFloat(const std::string& name, float value) const
+	{
+		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+	}
+
+
+private:
+
+	std::string LoadShaderFile(const std::string& path)
+	{
+		std::ifstream file(path);
+		std::stringstream buffer;
+		buffer << file.rdbuf();
+		return buffer.str();
+	}
+
+	void checkCompileErrors(unsigned int shader, std::string type)
+	{
+		int success;
+		char infoLog[1024];
+		if (type != "PROGRAM")
+		{
+			glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+			if (!success)
+			{
+				glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+				std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+			}
+		}
+		else
+		{
+			glGetProgramiv(shader, GL_LINK_STATUS, &success);
+			if (!success)
+			{
+				glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+				std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+			}
+		}
+	}
+};
+
+#endif
--- LearnOpenGL/window.cpp
+++ LearnOpenGL/window.cpp
@@ -4,16 +4,13 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include "Shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-void CheckShaderCompile(unsigned int shader, const char* name);
-std::string LoadShaderFile(const std::string& path);
-
 
 const char* vertexShaderSource;
 const char* fragmentShaderSource;
-
 
 int main() 
 {
@@ -46,55 +43,19 @@ int main()
 
 	// vertext input
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // left  
-		 0.5f, -0.5f, 0.0f, // right 
-		 0.0f,  0.5f, 0.0f  // top   
+		// verts			//colors
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,// left  
+		 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,// right 
+		 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f// top   
 	};
+
+	Shader ourShader("shaders/TestShader.vert", "shaders/TestShader.frag");
 
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	std::string vertSrc = LoadShaderFile("shaders/TestShader.vert");
-	std::string fragSrc = LoadShaderFile("shaders/TestShader.frag");
-
-	vertexShaderSource = vertSrc.c_str();
-	fragmentShaderSource = fragSrc.c_str();
-
-	// vert shader setup
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	CheckShaderCompile(vertexShader, "vertex");
-
-	//frag shader setup
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	CheckShaderCompile(fragmentShader, "fragment");
-
-	//shader program
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	int success;
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success)
-	{
-		char infoLog[512];
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "shader program link error\n" << infoLog << std::endl;
-	}
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
 
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
@@ -103,8 +64,11 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 
 	//render loop
@@ -117,7 +81,7 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
+		ourShader.use();
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		
@@ -125,6 +89,9 @@ int main()
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 	
 	glfwTerminate();
 	return 0;
@@ -141,23 +108,3 @@ void processInput(GLFWwindow* window)
 		glfwSetWindowShouldClose(window, true);
 }
 
-void CheckShaderCompile(unsigned int shader, const char* name)
-{
-	int success = 0;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-
-	if (!success) {
-		char infoLog[512];
-		glGetShaderInfoLog(shader, sizeof(infoLog), nullptr, infoLog);
-		std::cout << "Shader compile error (" << name << "):\n" << infoLog << std::endl;
-	}
-
-}
-
-std::string LoadShaderFile(const std::string& path)
-{
-	std::ifstream file(path);
-	std::stringstream buffer;
-	buffer << file.rdbuf();
-	return buffer.str();
-}
